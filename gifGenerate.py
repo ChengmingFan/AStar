f88858754003142bdbbd97c295a3df48aed8fca3
FAN Chengming <1009976636@qq.com>
2021-11-17 11:46:16 +0800

    add comment

--- gifGenerate.py
+++ gifGenerate.py
@@ -9,6 +9,6 @@ def png2gif(source,gif_name,time):
         frames.append(imageio.imread(png))
     imageio.mimsave(gif_name,frames,'GIF',duration=time)
     print('finish')
-
+# 这里使用了绝对路径 要修改 不知道为什么使用相对路径会无效
 address = "D:\PycharmProjects\AStar\images"
 png2gif(address,"D:\PycharmProjects\AStar\gif\\result.gif",0.05)
